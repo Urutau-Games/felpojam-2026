c7919529765d71ac86c422218471663aae4b4da4
Lazaro Menezes <lazarodm@gmail.com>
2026-02-27 21:06:00 -0300

    Fix button not returning properly

--- source/screens/player_board/player_board.gd
+++ source/screens/player_board/player_board.gd
@@ -94,7 +94,9 @@ func _on_stamp_dropped(is_release = false) -> void:
 	for cursor in cursor_container.get_children():
 		cursor.queue_free()
 		
-	if is_release and GameManager.active_stamp:
+	var pressed_button = stamp_button_group.get_pressed_button()
+	
+	if is_release and pressed_button:
 		stamp_button_group.get_pressed_button().set_pressed_no_signal(false)
 
 # TODO: Move to an observable approach
